bd0a7910498e4805f732713e3949e08be88f5b4f
Thiago <thiagopapadoce@outlook.com>
2022-10-04 18:38:15 -0300

    testando primeira configuração de produtos dinamicos

--- home/views.py
+++ home/views.py
@@ -1,5 +1,41 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def pagina_home(request):
-    return render(request, 'home.html')
+
+    context = {
+
+        'produtos':[
+            {'nome': 'Carregador de celular', 'preco': '40,00'},
+            {'nome': 'Capinha de Celular ', 'preco': '50,00'},
+            {'nome': 'Fone de Ouvido ', 'preco': '32,00'},
+            {'nome': 'PowerBank ', 'preco': '84,50'},
+            {'nome': 'Fone de Ouvido ', 'preco': '32,00'},
+            {'nome': 'PowerBank ', 'preco': '84,50'},
+            {'nome': 'Capinha de Celular ', 'preco': '50,00'},
+            {'nome': 'Fone de Ouvido ', 'preco': '32,00'},
+            {'nome': 'PowerBank ', 'preco': '84,50'},
+            {'nome': 'Fone de Ouvido ', 'preco': '32,00'},
+            {'nome': 'PowerBank ', 'preco': '84,50'},
+            {'nome': 'PowerBank ', 'preco': '84,50'},
+            {'nome': 'Fone de Ouvido ', 'preco': '32,00'},
+            {'nome': 'PowerBank ', 'preco': '84,50'},
+            {'nome': 'Capinha de Celular ', 'preco': '50,00'},
+            {'nome': 'Fone de Ouvido ', 'preco': '32,00'},
+            {'nome': 'PowerBank ', 'preco': '84,50'},
+            {'nome': 'Fone de Ouvido ', 'preco': '32,00'},
+            {'nome': 'PowerBank ', 'preco': '84,50'},
+        ], 
+        
+        'filtros':[
+            {'descricao': 'Confeitaria'},
+            {'descricao': 'artigos de festas'},
+            {'descricao': 'Balão'},
+            {'descricao': 'Outros'},
+        ]
+        
+    }
+
+
+    return render(request, 'produtosHome.html', context)
